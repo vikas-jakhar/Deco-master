45d6fac3ca63b03b90467dbc06ee405da2c671d4
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-03-29 13:01:48 +0530

    navbar update

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,4 +7,21 @@ menuicon.addEventListener("click", function () {
 
 document.querySelector('.menuicon').addEventListener('click', (e) => {
     e.currentTarget.classList.toggle('is-active');
+});
+
+const scrollicon = document.querySelector(".scroll-top");
+window.addEventListener("scroll", () => {
+    if (document.documentElement.scrollTop > 700) {
+        scrollicon.style.display = "grid";
+    }
+    else {
+        scrollicon.style.display = "none";
+    }
+});
+
+scrollicon.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    })
 })
